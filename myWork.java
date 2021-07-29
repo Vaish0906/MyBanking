07ab07892ec0b9890b945a226467fe4712cde604
Vaish0906 <38150305+Vaish0906@users.noreply.github.com>
2021-07-29 11:31:38 +0530

    Add files via upload

---
+++ myWork.java
@@ -0,0 +1,13 @@
+public class myWork{
+
+	public static void main(String args[]){
+		work w=new work();
+		w.working();
+	}
+}
+
+class work{
+	void working(){
+		System.out.println("Vaishnavi is working.. ");
+	}
+}
